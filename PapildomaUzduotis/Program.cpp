09a2ec151ff5c0748ff67d6e2f895bed226ce4c9
Vidmantas <jaguaras999@gmail.com>
2021-01-17 09:34:38 +0200

    Sukurtas projektas ir pradiniai failai.

---
+++ PapildomaUzduotis/Program.cpp
@@ -0,0 +1,16 @@
+// Program.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Tekstinio failo Tekstas.txt nuskaitytu zodziu skaicius: " << 9 << "\n";
+    std::cout << "Suskaiciuotas zodziu skaicius: " << 9 << "\n";
+    std::cout << "Suskaiciuotas unikaliu zodziu skaicius: " << 9 << "\n";
+    std::cout << "Pasikartojanciu daugiau nei 1 karta skaicius: " << 9 << "\n";
+    std::cout << "Zodziai pasikartojantys daugiau nei 1 karta issaugoti faile Rezultatai.txt: \n";
+
+
+
+
+}
